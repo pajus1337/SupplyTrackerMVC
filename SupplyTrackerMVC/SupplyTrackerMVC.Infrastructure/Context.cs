30db2057114c7000fd54858380e56e9e7f28a2a7
PaJuS <pajus1337@gmail.com>
2024-04-08 18:47:40 +0200

    Add new Context class in the Infrastructure layer for database     context, inheriting from IdentityDbContext.
    - Add DbSet properties for domain models: Contact, ContactDetail, ContactDetailType, Product, ProductDetail, ProductType, DeliveryBranch, Receiver, Sender.
    - Implement constructor accepting DbContextOptions and override OnModelCreating method.
    - Add package reference to Microsoft.AspNetCore.Identity.EntityFrameworkCore version 8.0.3 in SupplyTrackerMVC.Infrastructure.csproj.

---
+++ SupplyTrackerMVC/SupplyTrackerMVC.Infrastructure/Context.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SupplyTrackerMVC.Domain.Model.Contacts;
+using SupplyTrackerMVC.Domain.Model.Products;
+using SupplyTrackerMVC.Domain.Model.Receivers;
+using SupplyTrackerMVC.Domain.Model.Senders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyTrackerMVC.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactDetail> ContactDetails { get; set; }
+        public DbSet<ContactDetailType> ContactDetailTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<DeliveryBranch> DeliveryBranches { get; set; }
+        public DbSet<Receiver> Receivers { get; set; }
+        public DbSet<Sender> Senders { get; set; }
+            
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder); 
+        }
+    }
+}
