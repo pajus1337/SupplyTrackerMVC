38f7804ab752973a9573adb1dd2cf13a89eb09af
PaJuS <pajus1337@gmail.com>
2024-05-14 19:02:52 +0200

    Update address and contact repository interfaces with async methods
    - Extend IAddressRepository and IContactRepository with asynchronous CRUD operations.

--- SupplyTrackerMVC/SupplyTrackerMVC.Domain/Interfaces/IAddressRepository.cs
+++ SupplyTrackerMVC/SupplyTrackerMVC.Domain/Interfaces/IAddressRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SupplyTrackerMVC.Domain.Model.Addresses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SupplyTrackerMVC.Domain.Interfaces
 {
     public interface IAddressRepository
     {
+        Task<bool> AddAddressAsync(Address address, CancellationToken cancellationToken);
+        Task<bool> UpdateAddressAsync(int addressId, CancellationToken cancellationToken);
+        Task<bool> DeleteAddressAsync(int addressId, CancellationToken cancellationToken);
+        Task<Address> GetAddressAsync(int addressId, CancellationToken cancellationToken);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
--- SupplyTrackerMVC/SupplyTrackerMVC.Domain/Interfaces/IContactRepository.cs
+++ SupplyTrackerMVC/SupplyTrackerMVC.Domain/Interfaces/IContactRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SupplyTrackerMVC.Domain.Model.Contacts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SupplyTrackerMVC.Domain.Interfaces
 {
     public interface IContactRepository
     {
+        Task<bool> AddContactAsync(Contact contact, CancellationToken cancellationToken);
+        Task<bool> DeleteAddressAsync(int contact, CancellationToken cancellationToken);
+        Task<bool> UpdateAddressAsync(Contact contact, CancellationToken cancellationToken);
+        Task<Contact> GetContactAsync(int contactId, CancellationToken cancellationToken);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
