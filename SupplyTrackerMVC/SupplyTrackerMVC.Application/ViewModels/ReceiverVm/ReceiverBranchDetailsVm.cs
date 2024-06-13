b6e2989908ebf2f975c23b4d753628f5f5022698
PaJuS <pajus1337@gmail.com>
2024-06-13 22:06:42 +0200

    Add ViewModels for Receiver Branch Details and Receiver Details
    ViewModel additions:
    - ReceiverBranchDetailsVm:
      - Added new ViewModel to represent detailed information of a Receiver Branch.
      - Includes properties for Id, Name, BranchInternalID, BranchAlias, AddressId, AddressDetails, ContactId, ContactDetails, ReceiverId, and ReceiverDetails.
      - Implemented Mapping method for AutoMapper configuration.
    
    - ReceiverDetailsForRbVm:
      - Added new ViewModel to represent basic details of a Receiver associated with a Receiver Branch.
      - Includes properties for Id and Name.
      - Implemented Mapping method for AutoMapper configuration.

---
+++ SupplyTrackerMVC/SupplyTrackerMVC.Application/ViewModels/ReceiverVm/ReceiverBranchDetailsVm.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using SupplyTrackerMVC.Application.Mapping;
+using SupplyTrackerMVC.Application.ViewModels.Common;
+using SupplyTrackerMVC.Domain.Model.Receivers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyTrackerMVC.Application.ViewModels.ReceiverVm
+{
+    public class ReceiverBranchDetailsVm : IMapFrom<ReceiverBranch>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BranchInternalID { get; set; }
+        public string BranchAlias { get; set; }
+
+        public int AddressId { get; set; }
+        public AddressDetailsVm AddressDetails { get; set; }
+
+        public int ContactId { get; set; }
+        public ContactDetailsVm ContactDetails { get; set; }
+        public int ReceiverId { get; set; }
+        public ReceiverDetailsForRbVm ReceiverDetails { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ReceiverBranch, ReceiverBranchDetailsVm>();
+        }
+    }
+}
---
+++ SupplyTrackerMVC/SupplyTrackerMVC.Application/ViewModels/ReceiverVm/ReceiverDetailsForRbVm.cs
@@ -0,0 +1,16 @@
+﻿using SupplyTrackerMVC.Application.Mapping;
+using SupplyTrackerMVC.Domain.Model.Receivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyTrackerMVC.Application.ViewModels.ReceiverVm
+{
+    public class ReceiverDetailsForRbVm : IMapFrom<Receiver>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
