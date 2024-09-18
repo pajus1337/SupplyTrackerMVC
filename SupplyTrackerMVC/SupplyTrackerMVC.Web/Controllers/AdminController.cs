7cd13c1b2904c40fa1c98c17bbd06f9807a069c8
PaJuS <pajus1337@gmail.com>
2024-09-18 05:15:48 +0200

    feat: Implement contact type management in AdminController and views
    - Added new views: AddContactType.cshtml, EditContactType.cshtml, and ViewContactTypesList.cshtml for managing contact detail types.
    - Updated AdminController with methods for adding, editing, and viewing contact detail types.
    - Upgraded the Microsoft.VisualStudio.Web.CodeGeneration.Design package version to 8.0.5.

--- SupplyTrackerMVC/SupplyTrackerMVC.Web/Controllers/AdminController.cs
+++ SupplyTrackerMVC/SupplyTrackerMVC.Web/Controllers/AdminController.cs
@@ -1,14 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SupplyTrackerMVC.Application.Interfaces;
+using SupplyTrackerMVC.Application.ViewModels.Common;
 
 namespace SupplyTrackerMVC.Web.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController(IAdminService adminService) : BaseController
     {
+        private readonly IAdminService _adminService = adminService;
+
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> ViewContactTypesList(CancellationToken cancellationToken)
+        {
+            // TODO: Refine method => since this belowe is just a prototype.
+            var model = await _adminService.GetListContactDetailTypeAsync(cancellationToken);
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddContactType(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContactType(AddContactDetailTypeVm model, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditContactType(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditContactType(UpdateContactDetailTypeVm model, CancellationToken cancellationToke)
+        {
+            throw new NotImplementedException();
+        }
+             
 
     }
 }
