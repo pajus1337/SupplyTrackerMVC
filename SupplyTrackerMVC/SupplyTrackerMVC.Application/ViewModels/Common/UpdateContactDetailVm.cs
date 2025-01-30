09413f7a6df3366c722b69293ae954e2c9c51131
pajus1337 <pajus1337@gmail.com>
2025-01-30 22:23:38 +0100

    Improve contact detail update form
    - **UpdateContactDetailVm**:
      - Add `[Display]` attributes for improved form labels.
    
    - **UpdateContactDetail.cshtml**:
      - Ensure `Id`, `ContactId`, and `ContactDetailTypeId` fields are properly hidden.
      - Improve dropdown structure for selecting a contact detail type.

--- SupplyTrackerMVC/SupplyTrackerMVC.Application/ViewModels/Common/UpdateContactDetailVm.cs
+++ SupplyTrackerMVC/SupplyTrackerMVC.Application/ViewModels/Common/UpdateContactDetailVm.cs
@@ -4,6 +4,7 @@ using SupplyTrackerMVC.Application.Mapping;
 using SupplyTrackerMVC.Domain.Model.Contacts;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,8 +17,10 @@ namespace SupplyTrackerMVC.Application.ViewModels.Common
         public int ContactId { get; set; }
         public int ContactDetailTypeId { get; set; }
         // TODO: Add Collection of contactDetailTypes to create a select list element in view section.
+        [Display(Name = "Contact Detail Type")]
         public List<ContactDetailTypeForListVm> ContactDetailTypes { get; set; }
         public string ContactDetailTypeName { get; set; }
+        [Display(Name = "Contact Detail Value")]
         public string ContactDetailValue { get; set; }
 
         public void Mapping(Profile profile)
