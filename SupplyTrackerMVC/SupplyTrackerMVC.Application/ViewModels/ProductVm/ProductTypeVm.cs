0d59d7012e20ad59f6fa65f791f30bb7a6156257
pajus1337 <pajus1337@gmail.com>
2025-03-27 23:59:59 +0100

    feat: add display names to ProductTypeVm properties

--- SupplyTrackerMVC/SupplyTrackerMVC.Application/ViewModels/ProductVm/ProductTypeVm.cs
+++ SupplyTrackerMVC/SupplyTrackerMVC.Application/ViewModels/ProductVm/ProductTypeVm.cs
@@ -3,6 +3,7 @@ using SupplyTrackerMVC.Application.Mapping;
 using SupplyTrackerMVC.Domain.Model.Products;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,12 @@ namespace SupplyTrackerMVC.Application.ViewModels.ProductVm
     public class ProductTypeVm : IMapFrom<ProductType>
     {
         public int Id { get; set; }
+        [Display(Name = "Physical State")]
         public string PhysicalState { get; set; }
+        [Display(Name = "ADR Product")]
         public bool IsADRProduct { get; set; }
-
+        [Display(Name = "Packaged")]
+        public bool IsPackaged { get; set; }
 
         public void Mapping(Profile profile)
         {
